24af97619fd7935f4ee2c8519d38ca2c4203a930
elguapo87 <pglogovac@gmail.com>
2025-10-30 21:55:19 +0100

    Added real data & addPost API function in CreatePost page

--- client/src/app/auth/createPost/page.tsx
+++ client/src/app/auth/createPost/page.tsx
@@ -1,21 +1,95 @@
 "use client"
 
-import { useState } from "react"
-import { dummyUserData } from "../../../../public/assets";
+import { useContext, useState } from "react"
 import Image from "next/image";
 import { X, Image as ImageIcon } from "lucide-react";
+import axios from "@/lib/axios";
+import { ImageKitClient } from "imagekitio-next";
 import toast from "react-hot-toast";
+import { UserContext } from "@/context/UserContext";
+import { assets } from "../../../../public/assets";
+
+interface MyImageKitOptions {
+    publicKey: string;
+    urlEndpoint: string;
+    authenticationEndpoint: string;
+};
 
 const CreatePost = () => {
+
+    const context = useContext(UserContext);                                           
+    if (!context) throw new Error("CreatePost must be within UserContextProvider");
+    const { user } = context;
 
     const [content, setContent] = useState("");
     const [images, setImages] = useState<File[]>([]);
     const [loading, setLoading] = useState(false);
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
 
-    const user = dummyUserData;
+        try {
+            setLoading(true);
+            await axios.get("/sanctum/csrf-cookie");
 
-    const handleSubmit = async () => {
+            const imageKit = new ImageKitClient({
+                publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+                urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+                authenticationEndpoint: "http://localhost:8000/api/imagekit-auth",
+            } as MyImageKitOptions);
 
+            const uploadUrls: string[] = [];
+            for (const image of images) {
+                // fetch fresh token per upload
+                const { data: authData } = await axios.get("/api/imagekit-auth");
+
+                const uploadRes = await imageKit.upload({
+                    file: image,
+                    fileName: `${Date.now()}_${image.name}`,
+                    folder: "/monolith/posts",
+                    signature: authData.signature,
+                    token: authData.token,
+                    expire: authData.expire,
+                });
+
+                uploadUrls.push(uploadRes.url);
+            }
+
+            let postType: "text" | "image" | "text_with_image" = "text";
+            if (uploadUrls.length  > 0 && content.trim() !== "") {
+                postType = "text_with_image";
+
+            } else if (uploadUrls.length > 0) {
+                postType = "image";
+            }
+
+            if (content.trim() === "" && uploadUrls.length === 0) {
+                toast.error("You can't publish an empty post");
+                setLoading(false);
+                return;
+            }
+
+            const { data } = await axios.post("/api/posts", {
+                content,
+                post_type: postType,
+                image_urls: uploadUrls
+            });
+            if (data.success) {
+                toast.success(data.message);
+                setContent("");
+                setImages([]);
+
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            console.error("Create post error:", error);
+            toast.error("Failed to create post");
+
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -31,10 +105,16 @@ const CreatePost = () => {
                 <div className="max-w-xl bg-white p-4 sm:p-8 sm:pb-3 rounded-xl shadow-md space-y-4">
                     {/* HEADER */}
                     <div className="flex items-center gap-3">
-                        <Image src={user.profile_picture} alt="" className="w-12 h-12 rounded-full shadow" />
+                        <Image
+                            src={user?.profile_picture || assets.avatar_icon} 
+                            alt=""
+                            width={48}
+                            height={48} 
+                            className="w-12 h-12 rounded-full shadow" 
+                        />
                         <div>
-                            <h2 className="font-semibold">{user.full_name}</h2>
-                            <p className="text-sm text-gray-500">@{user.username}</p>
+                            <h2 className="font-semibold">{user?.full_name}</h2>
+                            <p className="text-sm text-gray-500">@{user?.user_name}</p>
                         </div>
                     </div>
 
@@ -97,21 +177,15 @@ const CreatePost = () => {
                         />
 
                         <button
-                            onClick={() => toast.promise(
-                                handleSubmit(),
-                                {
-                                    loading: "uploading...",
-                                    success: <p>Post Added</p>,
-                                    error: <p>Post Not Added</p>,
-                                }
-                            )}
-                            disabled={loading} 
+                            onClick={handleSubmit}
+                            disabled={loading || (content.trim() === "" && images.length === 0)} 
                             type="submit"
-                            className="text-sm bg-linear-to-r from-indigo-500 to-purple-600
+                            className={`text-sm bg-linear-to-r from-indigo-500 to-purple-600
                             hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white
-                            font-medium px-8 py-2 rounded-md cursor-pointer"
+                            font-medium px-8 py-2 rounded-md cursor-pointer ${(loading || (content.trim() === "" 
+                                && images.length === 0)) ? "opacity-70 cursor-not-allowed" : ""}`}
                         >
-                            Publish Post
+                            {loading ? "Publishing..." : "Publish Post"}
                         </button>
                     </div>
                 </div>
